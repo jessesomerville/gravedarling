1c0590346e98578d2219eaebbfa686af9631f76d
jessesomerville <jssomerville2@gmail.com>
2023-09-07 17:29:27 -0700

    Remove jquery and make animation more random

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,47 @@
-$(function () {
-  $('.blob').each(function () {
-    console.log($(this).position());
-    const dx = (Math.random() * 200) - 100;
-    const dy = (Math.random() * 100) - 50;
-    const scale = 1.5 - Math.random();
-    const rot = Math.random() * 180;
-    const dur = (Math.random() * 20) + 10;
-    $(this).css({
-      'transform': `translate(${dx}%, ${dy}%) scale(${scale}) rotate(${rot}deg)`,
-      'animation-duration': `${dur}s`,
-    });
-  });
+const blurScale = 120;
+const bezierScale = 2750;
+
+document.addEventListener('DOMContentLoaded', function () {
+  scaleGooBlur();
+  const blobs = document.querySelectorAll('.blob');
+  for (const e of blobs) {
+    e.style.setProperty('--diameter', randomInRange(8, 12) + '%');
+
+    e.style.setProperty('--a1', randomInRange(0, 1) + 'deg');
+    e.style.setProperty('--a2', randomInRange(0, 1) + 'deg');
+    e.style.setProperty('--d1', randomInRange(0, 0.9) + 'px');
+    e.style.setProperty('--d2', randomInRange(0, 0.9) + 'px');
+    e.style.setProperty('--da1', randomInRange(9, 30) + 's');
+    e.style.setProperty('--da2', randomInRange(9, 30) + 's');
+    e.style.setProperty('--dd1', randomInRange(9, 30) + 's');
+    e.style.setProperty('--dd2', randomInRange(9, 30) + 's');
+  }
 });
+
+window.addEventListener('resize', function () {
+  scaleGooBlur();
+  const blobs = document.querySelectorAll('.blob');
+  for (const e of blobs) {
+    const d1a = scaleGooBezier(e.style.getPropertyValue('--d1'));
+    const d2a = scaleGooBezier(e.style.getPropertyValue('--d2'));
+    e.style.setProperty('--d1', d1a);
+    e.style.setProperty('--d2', d2a);
+  }
+});
+
+function scaleGooBlur() {
+  const σ = window.innerWidth / blurScale;
+  document.getElementById('goo-blur').setAttribute('stdDeviation', σ);
+}
+
+function scaleGooBezier(d1) {
+  const d1a = d1.slice(0, -2);
+  if (Number.isNaN(Number.parseFloat(d1a))) {
+    return d1;
+  }
+  return parseFloat(d1a) / bezierScale + 'px';
+}
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
